7b1295d564d6899e41789781609c378a72f71684
aelhaddadi <contact@aelhaddadi.com>
2026-02-19 15:11:24 +0000

    Updates in lazy

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,15 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
 	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = { "typescript", "tsx", "javascript", "lua", "dart", "html" },
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+		indent = { enable = true },
+	},
 }
